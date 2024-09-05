be6ea5f061b1d8fb1276d08b5e5c571185be0fe8
saika <sxp42750@ucmo.edu>
2024-09-05 09:47:47 -0400

    Concatnate two strings

---
+++ src/main/scala/Concatenate.scala
@@ -0,0 +1,8 @@
+object Concatenate {
+  def main(args: Array[String]): Unit = {
+    var a = "Hi my name is kaushik"
+    var b = "I am learning strings"
+    var result = a.concat(" ").concat(b)
+    println(result)
+  }
+  }
